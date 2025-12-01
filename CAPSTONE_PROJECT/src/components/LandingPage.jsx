58962ee4cf17ec237accf8ade5965088cd401ce7
zanele maseko <itszanele@gmail.com>
2025-12-01 16:40:43 +0200

    final landing page layout and styling

--- CAPSTONE_PROJECT/src/components/LandingPage.jsx
+++ CAPSTONE_PROJECT/src/components/LandingPage.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 export function LandingPage() {
   return (
-    <div>
-      <div className='bg-blue-500 w-screen h-screen mx-auto'> first layer
-        <div className='bg-red-500 w-410 h-172 mx-auto'> second layer
-            <div className='bg-orange-500 w-400 h-162 mx-auto'> third layer
-                <div className='bg-yellow-500 w-390 h-152 mx-auto'> fourth layer
-                    <div className='bg-pink-500 w-380 h-142 mx-auto'>  fifth layer
+    <div className='flex justify-center align-center'>
+      <div className='bg-[#00008B] w-screen h-screen mx-auto rounded-xl '> 
+        <div className='bg-[#0000CD] w-300 h-170 mx-auto rounded-xl '> 
+            <div className='bg-[#4169E1] w-250 h-150 mx-auto rounded-xl '>
+                <div className='bg-[#1E9OFF]  w-200 h-100 mx-auto rounded-xl '>
+                    <div className='bg-[#00BFFF] w-160 h-80 mx-auto rounded-xl flex flex-col justify-between items-center pt-5 pb-5'>
+                       <h1 className='text-xl font-bold font-sans'>Welcome! <br />
+                          Take a quiz to test your general knowledge.
+                          Are you up for the challenge?
+                        </h1>
 
+                       <p className='mt-10 text-xl font-sans'>Enter your name:</p>
+                       <input type="text" className='bg-white w-80 rounded-xl p-3' placeholder='Enter your name:'/>
+                       <br />
+                       <button className='bg-white rounded-xl w-30 h-10 text-center font-bold text-normal'>lets Go!!</button>
                     </div>
 
                 </div>
