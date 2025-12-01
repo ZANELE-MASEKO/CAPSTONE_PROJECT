0451b06e0677cf2e26b8224c47f52336e8bc6538
zanele maseko <itszanele@gmail.com>
2025-12-01 15:03:58 +0200

    first page

--- CAPSTONE_PROJECT/src/App.jsx
+++ CAPSTONE_PROJECT/src/App.jsx
@@ -1,27 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
+import { LandingPage } from './components/LandingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div class='bg-blue-500 max-w-1800px max-h-800px mx-auto flex justify-center align-center'>
-      first layer
-      <div class='bg-red-500 w-1750px max-h-750px mx-auto flex justify-center align-center'>
-        second layer
-        <div>
-            third layer
-            <div>
-              fourth layer
-              <div>
-                last layer
-              </div>
-            </div>
-        </div>
-      </div>
+    <div class=' bg-white-500 flex justify-center text-center align-cneter'>
+      <LandingPage/>
     </div>
   )
 }
