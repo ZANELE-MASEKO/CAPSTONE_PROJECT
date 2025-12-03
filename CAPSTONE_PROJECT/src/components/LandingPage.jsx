421c2d0b5e77895351fac47b9dd63f5be07bdceb
zanele maseko <itszanele@gmail.com>
2025-12-03 12:47:52 +0200

    final layout for landing page

--- CAPSTONE_PROJECT/src/components/LandingPage.jsx
+++ CAPSTONE_PROJECT/src/components/LandingPage.jsx
@@ -2,27 +2,46 @@ import React from 'react';
 
 export function LandingPage() {
   return (
-    <div className='flex justify-center align-center'>
-      <div className='bg-[#00008B] w-screen h-screen mx-auto rounded-xl '> 
-        <div className='bg-[#0000CD] w-300 h-170 mx-auto rounded-xl '> 
-            <div className='bg-[#4169E1] w-250 h-150 mx-auto rounded-xl '>
-                <div className='bg-[#1E9OFF]  w-200 h-100 mx-auto rounded-xl '>
-                    <div className='bg-[#00BFFF] w-160 h-80 mx-auto rounded-xl flex flex-col justify-between items-center pt-5 pb-5'>
+    <div className='bg-[#00008B] w-screen h-screen mx-auto flex-grid justify-center items-center flex'>
+
+      <div className='bg-[#0000CD] w-300 h-170 mx-auto rounded-xl flex flex-grid justify-center items-center'> 
+   
+                    <div className='bg-[#00BFFF] w-250 h-150 mx-auto rounded-xl flex flex-col justify-evenly items-center pt-5 pb-5'>
                        <h1 className='text-xl font-bold font-sans'>Welcome! <br />
                           Take a quiz to test your general knowledge.
                           Are you up for the challenge?
                         </h1>
 
-                       <p className='mt-10 text-xl font-sans'>Enter your name:</p>
+                       <p className='mt-10 text-xl font-sans mb-[-14px]'>Enter your name:</p>
+                    
                        <input type="text" className='bg-white w-80 rounded-xl p-3' placeholder='Enter your name:'/>
-                       <br />
-                       <button className='bg-white rounded-xl w-30 h-10 text-center font-bold text-normal'>lets Go!!</button>
+                       
+                       <div className='w-150 h-30 flex justify-center items-center flex-row justify-evenly rounded-xl gap-30 '>
+
+                        <div>
+                            <label className='bg-white w-50 h-6 flex justify-center items-center rounded-tl-[10px] rounded-tr-[10px]'
+                            >Choose Topic</label>
+
+                            <select className='bg-white w-50 h-6 rounded-bl-[10px] rounded-br-[10px] hover:bg-[#78cdf5]'>
+                              <option value="">1</option>
+                              <option value="">1</option>
+                              <option value="">1</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <label className='bg-white w-50 h-6 flex justify-center items-center rounded-tl-[10px] rounded-tr-[10px]'
+                            >No Questions</label>
+                            <select className='bg-white w-50 h-6 rounded-bl-[10px] rounded-br-[10px] hover:bg-[#78cdf5]'>
+                              <option value="">1</option>
+                              <option value="">1</option>
+                              <option value="">1</option>
+                            </select>
+                        </div>
+                       </div>
+                       <button className='bg-white rounded-xl w-30 h-10 text-center font-bold text-normal hover:bg-[#78cdf5]'>lets Go!!</button>
+                       
                     </div>
-
-                </div>
-
-            </div>
-        </div>
 
       </div>
        
