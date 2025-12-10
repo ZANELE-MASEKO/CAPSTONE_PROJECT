55184988820e0c46fc8171243fd7cc2805ed33eb
zanele maseko <itszanele@gmail.com>
2025-12-10 18:48:10 +0200

    added state for API usage

--- CAPSTONE_PROJECT/src/components/QuizPage.jsx
+++ CAPSTONE_PROJECT/src/components/QuizPage.jsx
@@ -1,10 +1,32 @@
-import React  from "react";
+import React, { use }  from "react";
 import {useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 
 export function QuizPage() {
 
     const navigate= useNavigate();
+
+    //state management for quiz questions
+    //var for all quiz questions
+    const [questions, setQuestions] = useState([]);
+    //var for the current question
+    const [currentQuestion, setCurrentQuestion]= useState(0);
+    //var will show the funal score
+    const [showScore, setashowScore] =useState(false);
+    //var to keep track of the score
+    const [score, setScore]= useState(0);
+    //var keeps trach of selected answer by user
+    const [selectedAnswer, setSelectedAnswer]= useState([]);
+
+    //api questions
+    const apiQuestions = async() => {
+        const apiRespone = await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
+        //convert api data into jsn() data
+        const apiData= await apiRespone.json();
+        console.log(data);
+        //all fetched questions from the api will be store in questions var
+        setQuestions(apiData);
+    }
 
     return(
         <div className='bg-[#00008B] w-screen h-screen mx-auto flex justify-center items-center flex-grid'>
@@ -16,7 +38,7 @@ export function QuizPage() {
                     <div className='bg-[#1E9OFF]  w-200 h-100 mx-auto rounded-xl flex justify-center items-center flex-grid'>
 
                         <div className='bg-[#00BFFF]  w-190 h-100 mx-auto rounded-xl flex justify-center items-center flex-col mt-50 justify-evenly'>
-                            <h1 className='text-xl font-bold font-sans'>Quiz Topic</h1>
+                            <h1 className='text-xl font-bold font-sans'>General Knowledge</h1>
                             <p>Quiz Question</p>
 
                             
