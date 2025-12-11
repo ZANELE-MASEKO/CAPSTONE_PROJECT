aa2af04a2c235a66f1b588b8b2634f3481fca5c3
zanele maseko <itszanele@gmail.com>
2025-12-11 15:46:25 +0200

    api questions displayed on UI

--- CAPSTONE_PROJECT/src/components/QuizPage.jsx
+++ CAPSTONE_PROJECT/src/components/QuizPage.jsx
@@ -2,6 +2,7 @@ import React, { use }  from "react";
 import {useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 
+
 export function QuizPage() {
 
     const navigate= useNavigate();
@@ -12,21 +13,70 @@ export function QuizPage() {
     //var for the current question
     const [currentQuestion, setCurrentQuestion]= useState(0);
     //var will show the funal score
-    const [showScore, setashowScore] =useState(false);
+    const [showScore, setShowScore] =useState(false);
     //var to keep track of the score
     const [score, setScore]= useState(0);
     //var keeps trach of selected answer by user
     const [selectedAnswer, setSelectedAnswer]= useState([]);
 
     //api questions
-    const apiQuestions = async() => {
-        const apiRespone = await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
+    const fetchQuestions = async() => {
+
+        try{
+        const respone = await fetch('https://quizapi.io/api/v1/questions?limit=10&category=code', 
+             {
+            headers:{
+                'X-Api-KEY':'6YkxXAoPuHIBMOcs7tbW6WYbRbzTEbUW0CaPaDuo'
+            }
+        }
+        );
+
+
+        if(!respone.ok){
+            throw new Error('Failed to fetch questions');
+        }
+
         //convert api data into jsn() data
-        const apiData= await apiRespone.json();
-        console.log(data);
+        const apiData= await respone.json();
+        console.log(apiData);
         //all fetched questions from the api will be store in questions var
         setQuestions(apiData);
+        }
+        catch(error){
+            console.log(error);
+        }
     }
+
+    //this will looad th questions once the componnet is live/active
+    useEffect(() => {
+        fetchQuestions()
+    }, []);
+
+    const handleAnswerOptionClick= (isCorrect,answer) => {
+        //isCorrect is a boolean value, if its true the score gets increased by 1
+        if(isCorrect) {
+            setScore((prev) => prev +1);
+        }
+
+        const updatedSelecetdAnswers= [...selectedAnswer];
+        updatedSelecetdAnswers[currentQuestion]= answer;
+        setSelectedAnswer(updatedSelecetdAnswers);
+
+        const nextQuestion= currentQuestion+1 
+        if (nextQuestion <questions.length) {
+            setCurrentQuestion(nextQuestion)
+        } else 
+            setShowScore(true)
+    }
+
+    const handlePlayAgainClick= () => {
+        //this will reset all the states to initial values
+        setCurrentQuestion(0);
+        setScore(0);
+        setShowScore(false);
+        setSelectedAnswer([]);
+    }
+
 
     return(
         <div className='bg-[#00008B] w-screen h-screen mx-auto flex justify-center items-center flex-grid'>
@@ -37,9 +87,42 @@ export function QuizPage() {
 
                     <div className='bg-[#1E9OFF]  w-200 h-100 mx-auto rounded-xl flex justify-center items-center flex-grid'>
 
-                        <div className='bg-[#00BFFF]  w-190 h-100 mx-auto rounded-xl flex justify-center items-center flex-col mt-50 justify-evenly'>
+                        <div className='bg-[#00BFFF]  w-200 h-100 mx-auto rounded-xl flex justify-center items-center flex-col mt-50 justify-evenly'>
                             <h1 className='text-xl font-bold font-sans'>General Knowledge</h1>
-                            <p>Quiz Question</p>
+
+                            <hr/>
+                            {showScore ? (
+                                //if shwoScorore is true, final score will be displayed
+                                <div>
+                                    <h2>Your Total is: {score}/{questions.length}</h2>
+                                    <button
+                                    className='play-agin'
+                                    onClick={handlePlayAgainClick}>Play Again</button>
+                                </div>
+                            ): (
+                                //else, the question screen will be displayed
+                                <div >
+                                    <div className='question-section flex flex-col justify-center items-center gap-5'>
+                                        <div className='question-count'>
+                                            <span>{currentQuestion +1}</span>/{questions.length}
+                                        </div>
+
+                                        <div className="question-text">
+                                            {questions[currentQuestion]?.question}
+                                        </div>
+
+                                        {/*next and previous buttons*/}
+                                        <div className="flex gap-10">
+                                           {currentQuestion && (<button onClick={() =>setCurrentQuestion (currentQuestion-1)} className='bg-white rounded-xl w-30 h-10 text-center font-bold text-normal'>Previous</button>)} 
+                                           
+                                           {currentQuestion <questions.length && (<button onClick={() =>setCurrentQuestion (currentQuestion+1)} className='bg-white rounded-xl w-30 h-10 text-center font-bold text-normal'>Next</button>)} 
+                                        </div>
+
+                                    </div>
+                                </div>
+                            )
+
+                            }
 
                             
                             <button className='bg-white rounded-xl w-30 h-10 text-center font-bold text-normal' onClick={() => navigate('/total')}>
